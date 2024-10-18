5eca83e10bb1ec33b6eb2ee59faee8556958a9a1
JeremyDwayne <jeremy@jeremydwayne.com>
2024-10-18 13:43:04 -0400

    mysql driver

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -24,6 +27,13 @@ func main() {
 		staticDir: os.Getenv("STATIC_DIR"),
 	}
 
+	db, err := openDB(os.Getenv("DATABASE_URL"))
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
 	app := &application{
 		logger: logger,
 		config: config,
@@ -31,7 +41,22 @@ func main() {
 
 	logger.Info("Starting server", "addr", config.addr)
 
-	err := http.ListenAndServe(config.addr, app.routes())
+	err = http.ListenAndServe(config.addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
